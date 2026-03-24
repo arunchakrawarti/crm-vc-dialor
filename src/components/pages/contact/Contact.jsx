dbf8b13831951f928f9e2b1c1f6bfc28b0fbb4a2
arunchakrawarti <arunmojampur@gmail.com>
2026-03-24 15:20:21 +0530

    feat:contact module is completed

---
+++ src/components/pages/contact/Contact.jsx
@@ -0,0 +1,85 @@
+"use client";
+import BasicTable from '@/components/common/BasicTable';
+import Button from '@/components/common/Button';
+import RecordStatus from '@/components/common/RecordStatus';
+import React, { useState } from 'react';
+import NewContactModal from './NewContactModal';
+import AssignContactModal from './AssignContactModal';
+
+
+const Contact = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isModal, setIsModal] = useState(false);
+  const columns = [
+    { 
+      label: <input type="checkbox" className="w-4 h-4" />, 
+      key: "checkbox",
+      render: () => <input type="checkbox" className="w-4 h-4" />
+    },
+    { label: "S.No.", key: "srNo" },
+    { label: "Name", key: "name" },
+    { label: "Mobile", key: "mobile" },
+    { label: "Email", key: "email" },
+    { label: "Added by", key: "addedBy" },
+    { label: "Assigned To", key: "assignedTo" },
+    { label: "Comment", key: "comment" },
+    { label: "Date", key: "date" },
+  ];
+
+  
+  const data = []; 
+
+  return (
+    <div className="p-6 bg-white rounded-lg shadow-sm border border-gray-100 font-inter">
+      <div className="flex justify-between items-center mb-6">
+        <Button 
+          variant="outline" 
+          className="border-gray-200 px-6 py-2 shadow-sm text-red-600 font-bold uppercase tracking-wider text-xs bg-white"
+        >
+          Watch Video
+          <i className="ri-youtube-fill text-red-600 text-lg ml-1"></i>
+        </Button>
+
+        <div className="flex gap-1">
+          <Button 
+          onClick={() => setIsOpen(true)}
+            variant="success"
+          >
+            New Contact
+          </Button>
+          <Button 
+          onClick={() => setIsModal(true)}
+            variant="success"
+             
+          >
+            Assign
+          </Button>
+        </div>
+      </div>
+      <BasicTable 
+        columns={columns} 
+        data={data} 
+        wrapperClassName="rounded-sm border-gray-200"
+      />
+      <div className="mt-2">
+        <RecordStatus 
+          totalItems={0} 
+          currentPage={1} 
+          itemsPerPage={30} 
+          label="Contacts"
+          onPageChange={(page) => console.log(page)}
+        />
+      </div>
+<NewContactModal 
+        isOpen={isOpen} 
+        onClose={() => setIsOpen(false)} 
+      />
+      <AssignContactModal 
+        isOpen={isModal} 
+        onClose={() => setIsModal(false)} 
+      />
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/pages/contact/NewContactModal.jsx
@@ -0,0 +1,95 @@
+"use client";
+import Button from '@/components/common/Button';
+import Input from '@/components/common/Input';
+import OverlayModal from '@/components/common/OverlayModal';
+import React from 'react';
+
+const NewContactModal = ({ isOpen, onClose }) => {
+
+  return (
+    <OverlayModal isOpen={isOpen} onClose={onClose}>
+        <div className="flex items-center justify-center">
+      <div className="w-full max-w-5xl bg-white rounded-lg shadow-2xl my-8 font-inter">
+        
+        <div className="flex items-center justify-between px-6 py-4 border-b border-gray-100">
+          <h2 className="text-2xl font-semibold text-gray-800">Add New Contact</h2>
+        </div>
+
+        <div className="p-8 overflow-y-auto h-120">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-5">
+            
+            <div className="flex gap-4 items-end">
+              <div className="w-1/3">
+                <Input label="Title" type="select" options={['Mr', 'Ms', 'Mrs', 'Dr']} placeholder="Mr" />
+              </div>
+              <div className="flex-1 relative">
+                <Input label="Name" placeholder="Enter name" />
+                <Button variant='success' className="absolute right-[35px] bottom-2 text-white rounded-full w-2 h-6">
+                  <i className="ri-add-line"></i>
+                </Button>
+              </div>
+            </div>
+
+            <div className="flex gap-4 mt-2 items-end">
+              <div className="w-1/3">
+                <Input label="Country Code" type="select" options={['India (+91)', 'USA (+1)']} placeholder="India (+91)" />
+              </div>
+              <div className="flex-1 relative">
+                <Input label="Mobile No" placeholder="Enter mobile no" />
+                <Button variant="success" className="absolute right-[-35px] bottom-2 text-white rounded-full w-2 h-6">
+                  <i className="ri-add-line"></i>
+                </Button>
+              </div>
+            </div>
+
+            <div className="relative mr-8">
+               <Input label="Source" type="select" placeholder="-Select Source-" options={[]} />
+            </div>
+            <div className="relative mr-8">
+              <Input label="Email" placeholder="Enter email" />
+              <Button variant='success' className="absolute right-[-35px] bottom-2 text-white rounded-full w-2  h-6">
+                <i className="ri-add-line"></i>
+              </Button>
+            </div>
+
+            <Input label="Website" placeholder="Enter website" />
+            <Input label="Owner" type="select" placeholder="Abhishek Jaiswal" options={['Abhishek Jaiswal']} />
+
+            <Input label="Position" type="select" placeholder="Unknown" options={['Unknown']} />
+            <Input label="Industry" type="select" placeholder="Unknown" options={['Unknown']} />
+
+            <Input label="facebook Profile" placeholder="Enter facebook profile" />
+            <Input label="Twitter Profile" placeholder="Enter twitter profile" />
+
+            <div className="grid grid-cols-3 gap-4 md:col-span-2">
+               <Input label="Select Country" type="select" placeholder="India" options={['India']} />
+               <Input label="Select State" type="select" placeholder="-Select State-" options={[]} />
+               <Input label="Select City" type="select" placeholder="-Select City-" options={[]} />
+            </div>
+
+            <div className="md:col-span-2">
+              <Input label="Comment" placeholder="Enter comment" className="h-20" />
+            </div>
+            <div className="md:col-span-2">
+              <Input label="Full Address" placeholder="Enter full address" className="h-20" />
+            </div>
+
+          </div>
+
+          <div className="mt-8 flex justify-end">
+            <Button 
+              variant="success"
+            >
+              <span className="font-bold tracking-widest text-sm uppercase">Add Contact</span>
+              <i className="ri-send-plane-fill ri-lg ml-2"></i>
+            </Button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    </OverlayModal>
+  );
+};
+
+export default NewContactModal;
