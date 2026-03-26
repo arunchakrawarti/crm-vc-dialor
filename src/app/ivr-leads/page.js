9b030440a32f0e5364da92f3123f1954a25f264d
arunchakrawarti <arunmojampur@gmail.com>
2026-03-26 16:17:30 +0530

    feat:Ivr module is completed

---
+++ src/app/ivr-leads/page.js
@@ -0,0 +1,15 @@
+import IvrLeads from '@/components/pages/ivr-leads/IvrLeads'
+import MainLayout from '@/components/templates/MainLayout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <MainLayout>
+        <IvrLeads/>
+      </MainLayout>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/pages/ivr-leads/IvrLeads.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { Monitor } from "lucide-react";
+
+const IvrLeads = () => {
+  return (
+    <div className="bg-white  rounded-md shadow-sm min-h-[450px] flex flex-col">
+      <div className="border-b border-blue-800 py-3 px-4">
+        <h1 className="text-center text-blue-600 font-medium text-lg">
+          IVR Leads
+        </h1>
+      </div>
+      <div className="flex-1 flex flex-col items-center justify-center text-center p-6">
+        <div className="mb-6 text-blue-600">
+          <Monitor size={100} strokeWidth={1} />
+        </div>
+
+        <p className="text-gray-500 text-xl">
+          You have no records found.
+        </p>
+      </div>
+
+    </div>
+  );
+};
+
+export default IvrLeads;
